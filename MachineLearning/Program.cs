8407e4ece6dcc6c20571740b31d53ec4c835df93
Interkosms <90975304+Interkosms@users.noreply.github.com>
2021-07-13 08:50:12 +0000

    delete tmp files

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -18,4 +18,4 @@ var estimator = ctx.Transforms.Text
     .FeaturizeText(
         outputColumnName: "Features",
         inputColumnName: nameof(SentimentData.Text)
-    ).Append(ctx.BinaryClassification.Trainers.SdcaLogisticRegression(f
+    ).Append(ctx.BinaryClassification.Trainers.SdcaLogisticRegression(featureCol
