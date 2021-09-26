b97ed8694d200834da9698477c34a7305713d722
Interkosms <90975304+Interkosms@users.noreply.github.com>
2021-09-26 11:32:59 +0000

    Modif mineure

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -30,4 +30,4 @@ Console
     {
         // training happens here
         model = estimator.Fit(splitDataView.TrainSet);
-        var predictions = model.Transf
+        var predictions = model.Transform(spli
