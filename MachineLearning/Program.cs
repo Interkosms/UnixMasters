4bbbd5dc2b76cb8502db5edfdae2bcb53235509d
Interkosms <90975304+Interkosms@users.noreply.github.com>
2021-10-06 13:18:12 +0000

    removed .DS_Store

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -32,4 +32,4 @@ Console
         model = estimator.Fit(splitDataView.TrainSet);
         var predictions = model.Transform(splitDataView.TestSet);
 
-        rule.Title = "🏁 
+        rule.Title = "🏁 Train
