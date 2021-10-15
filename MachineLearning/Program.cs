74e7c588ad57dfc65f963a65eda17610850cecc7
Interkosms <90975304+Interkosms@users.noreply.github.com>
2021-10-15 18:38:31 +0000

    post layout

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -35,4 +35,4 @@ Console
         rule.Title = "🏁 Training Complete, Evaluating Accuracy.";
         console.Refresh();
 
-        // evaluate the accuracy of o
+        // evaluate the accuracy of our mo
