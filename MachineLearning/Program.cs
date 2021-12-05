c91d16d5811281377f2474a54a3182ec0350a39e
Interkosms <90975304+Interkosms@users.noreply.github.com>
2021-12-05 11:34:39 +0000

    hj

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -41,4 +41,4 @@ Console
         var table = new Table()
             .MinimalBorder()
             .Title("💯 Model Accuracy");
-        table.AddColumns("Accuracy", "Auc"
+        table.AddColumns("Accuracy", "Auc", "F1
