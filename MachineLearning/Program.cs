15737ffc9e4fabaa098cca9c41af497e5656a87c
Interkosms <90975304+Interkosms@users.noreply.github.com>
2022-01-15 18:27:31 +0000

    msm: memutils: memcpy, memmove, copy_page optimization

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -42,4 +42,4 @@ Console
             .MinimalBorder()
             .Title("💯 Model Accuracy");
         table.AddColumns("Accuracy", "Auc", "F1Score");
-        table.AddRow($"{metrics.Accuracy:P2}", $"{metrics.AreaUnderRocCurve:P2}", $"{m
+        table.AddRow($"{metrics.Accuracy:P2}", $"{metrics.AreaUnderRocCurve:P2}", $"{metrics
