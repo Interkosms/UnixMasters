5ecbc972a3befbb0d239a4796f3cb003dc330de5
Interkosms <90975304+Interkosms@users.noreply.github.com>
2022-04-03 07:35:00 +0000

    ACPI sysfs.c strlen fix

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -51,4 +51,4 @@ Console
 while (true)
 {
     var text = AnsiConsole.Ask<string>("What's your [green]review text[/]?");
-    var engine = ctx.Model.CreatePredictionEngine<Sen
+    var engine = ctx.Model.CreatePredictionEngine<SentimentDa
