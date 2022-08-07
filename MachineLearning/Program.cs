902b4e9b070cb1a731801d4864e6270895306d15
Interkosms <90975304+Interkosms@users.noreply.github.com>
2022-08-07 09:40:51 +0000

    netfilter: qtaguid: initialize a local var to keep compiler happy.

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -65,4 +65,5 @@ while (true)
 class SentimentData
 {
     [LoadColumn(0)] public string? Text;
-    [LoadColumn(1), ColumnName("Label")] public bool Sentime
+    [LoadColumn(1), ColumnName("Label")] public bool Sentiment;
+}
