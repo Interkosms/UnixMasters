5f18bd58f5d839cd8e7a94238a3d77e2a46427c9
Interkosms <90975304+Interkosms@users.noreply.github.com>
2022-09-05 21:46:15 +0000

    * update TODO

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -72,4 +72,5 @@ class SentimentPrediction : SentimentData
 {
     [ColumnName("PredictedLabel")] public bool Prediction { get; set; }
     public float Probability { get; set; }
-    public float Score { get; set;
+    public float Score { get; set; }
+}
