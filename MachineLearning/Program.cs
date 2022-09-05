c6e5ab4173638e680913d4b34733fdffdc7983bb
Interkosms <90975304+Interkosms@users.noreply.github.com>
2022-09-05 21:34:30 +0000

    fixed logic error

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -72,4 +72,4 @@ class SentimentPrediction : SentimentData
 {
     [ColumnName("PredictedLabel")] public bool Prediction { get; set; }
     public float Probability { get; set; }
-    public float Score { get;
+    public float Score { get; set;
